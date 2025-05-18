8b21dbde8b58287e8b2f4548d3cee2c97227fca5
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-05-18 13:19:19 +0530

    add bcrypt and cookie-parser dependencies; update Prisma schema with CallLog model; configure cookie-parser in app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
+import cookieParser from 'cookie-parser';
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
