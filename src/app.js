fed8e162de101849fea252ca2cfcffa0760120fa
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-05-18 13:09:34 +0530

    add user routes and integrate with Express app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import userRoutes from './routes/user.routes.js';
+
 
 const app = express();
 app.use(cors());
@@ -9,5 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Hello World!');
     });
+app.use('/api', userRoutes);
 
 export default app;
---
+++ src/routes/user.routes.js
@@ -0,0 +1,8 @@
+import {Router} from "express"
+
+const router = Router()
+
+router.get("/user", (req, res) => {
+    res.send("User route")
+})
+export default router
