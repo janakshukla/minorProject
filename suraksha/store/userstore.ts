c22a7bfa019afd224801ceaff548d7382c76051f
janakshukla <148837744+janakshukla@users.noreply.github.com>
2025-05-19 10:04:38 +0530

    feat: add Zustand for state management and implement user store with AsyncStorage persistence

---
+++ suraksha/store/userstore.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+enum UserRole {
+    child,
+    parent,
+}
+
+interface User {
+    id: string;
+    name: string;
+    email: string;
+    role: UserRole;
+    createdAt: string;
+}
+interface UserStore {
+    user: User | null;
+    setUser: (user: User) => void;
+    clearUser: () => void;
+}
+
+export const useUserStore = create<UserStore>()(
+    persist(
+        (set) => ({
+            user: null,
+            setUser: (user: User) => set({ user }),
+            clearUser: () => set({ user: null }),
+        }),
+        {
+            name: "user-storage",
+            storage: createJSONStorage(() =>AsyncStorage),
+        }
+    )
+);
